595314824963cf351e92d543d89e08cd3af70793
mohdyaserkt <yaserkt786@gmail.com>
2023-10-20 20:08:11 +0530

    all service updated

--- services/invoice/src/index.ts
+++ services/invoice/src/index.ts
@@ -33,7 +33,7 @@ const start = async () => {
   }
 
   app.listen(intPort, () => {
-    console.log("Listening on port 4000 ....... ");
+    console.log("Listening on port 4000 ......... ");
   });
 };
 
