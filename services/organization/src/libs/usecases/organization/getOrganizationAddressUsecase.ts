75da1e8641b1c6003118afb46361c95e905131db
mohdyaserkt <yaserkt786@gmail.com>
2023-10-24 06:54:26 +0000

    24 Oct 2023, 12:24

---
+++ services/organization/src/libs/usecases/organization/getOrganizationAddressUsecase.ts
@@ -0,0 +1,26 @@
+
+import { DepenteniciesData } from "../../entities/interfaces";
+import bcrypt from "bcrypt";
+import {
+  IOrganization,
+  organizationRegistrationData,
+} from "../../entities/organization";
+
+export const createOrganization_UseCase = (dependencies: DepenteniciesData) => {
+  const {
+    repository: { organizationRepository,tenantRepository },
+  } = dependencies;
+
+  if (!organizationRepository)
+    throw new Error("The user repository should be dependencie");
+
+  const execute = async (comapanyName:string) => {
+    console.log(comapanyName,"company name");
+    
+    const model = await tenantRepository.getCompanySchema(comapanyName,"Organization")
+    return organizationRepository.findOrganizationAddress(model);
+  };
+  return {
+    execute,
+  };
+};
