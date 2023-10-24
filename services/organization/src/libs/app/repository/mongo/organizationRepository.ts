2a9c2dd041f3ea02c2e654d731dd7ed746740b2a
mohdyaserkt <yaserkt786@gmail.com>
2023-10-24 06:37:35 +0000

    24 Oct 2023, 12:07

--- services/organization/src/libs/app/repository/mongo/organizationRepository.ts
+++ services/organization/src/libs/app/repository/mongo/organizationRepository.ts
@@ -1,30 +1,33 @@
-
 import { IOrganization } from "../../../entities/organization";
 
 export const organizationRepository = {
-  findByEmail: async (email: string,organizationModel:any) => {
+  findByEmail: async (email: string, organizationModel: any) => {
     const user = await organizationModel.findOne({ email });
     return user ? user.toObject() : null;
   },
 
-  findOrganizations: async (id: string,organizationModel:any) => {
-    console.log("rea findorgrepo",id);
-    
-    const organizations = await organizationModel.find({userId:id})
-    console.log(organizations,"dfs");
+  findOrganizations: async (id: string, organizationModel: any) => {
+    console.log("rea findorgrepo", id);
+
+    const organizations = await organizationModel.find({ userId: id });
+    console.log(organizations, "dfs");
     return organizations ? organizations : null;
   },
 
-  createOrganization: async (Organization: IOrganization,organizationModel:any) => {
+  createOrganization: async (
+    Organization: IOrganization,
+    organizationModel: any
+  ) => {
     const createdOrganization = await organizationModel.create(Organization);
     return createdOrganization.toObject();
   },
 
-  findOrganizationAddress: async (organizationModel:any) => {
-    
-    
-    const organizations = await organizationModel.findOne({})
-    console.log(organizations,"dfs");
-    return organizations ? organizations : null;
+  findOrganizationAddress: async (organizationModel: any) => {
+    const organizationAddress = await organizationModel.findOne(
+      {},
+      { organizationAddress: 1 }
+    );
+    console.log(organizationAddress, "dfs");
+    return organizationAddress ? organizationAddress : null;
   },
 };
