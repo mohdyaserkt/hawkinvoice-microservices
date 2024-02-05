400b7bf5af48dc72910ca2c654b2257eb4d8b07b
mohdyaserkt <yaserkt786@gmail.com>
2024-02-05 16:14:47 +0530

    new cors added for development

--- services/organization/src/app.ts
+++ services/organization/src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.set("trust-proxy", true);
 app.use(json());
 app.use(cors({
-  origin: ['https://obscure-capybara-j9gw5jwrg54h9x6.github.dev', 'https://hawkinvoice-client.vercel.app'],
+  origin: ['https://obscure-capybara-j9gw5jwrg54h9x6.github.dev','https://hawkinvoice-client.vercel.app'],
   methods: ['GET', 'POST']
 }));
 app.use(
